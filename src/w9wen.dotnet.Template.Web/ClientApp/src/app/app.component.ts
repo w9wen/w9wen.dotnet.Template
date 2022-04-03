b12f7f65691b656fa500a8d8db24ba0453896062
w9wen <william.wen@yes-energy.com.tw>
2022-04-04 00:06:26 +0800

    temp($App) Create the http request.

--- src/w9wen.dotnet.Template.Web/ClientApp/src/app/app.component.ts
+++ src/w9wen.dotnet.Template.Web/ClientApp/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ProjectDto } from './_models/project-dto.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'W9WEN App';
+  project: ProjectDto;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getProject();
+  }
+  
+  getProject() {
+    this.http.get<ProjectDto>("http://localhost:57679/Projects").subscribe(response => {
+      this.project = response;
+    })
+  }
 }
