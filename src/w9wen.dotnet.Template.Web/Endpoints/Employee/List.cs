3e61e8ff5fd1129bc9b35366a7fae96fc99c98dd
w9wen <william.wen@yes-energy.com.tw>
2022-05-08 07:44:52 +0800

    Employee($Endpoint) Pagenation.

--- src/w9wen.dotnet.Template.Web/Endpoints/Employee/List.cs
+++ src/w9wen.dotnet.Template.Web/Endpoints/Employee/List.cs
@@ -2,16 +2,18 @@ using Ardalis.ApiEndpoints;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using src.w9wen.dotnet.Template.Web.Endpoints.Employee;
 using Swashbuckle.AspNetCore.Annotations;
 using w9wen.dotnet.Template.Infrastructure.Data;
 using w9wen.dotnet.Template.Web.Configurations;
+using w9wen.dotnet.Template.Web.Extensions;
 
 namespace w9wen.dotnet.Template.Web.Endpoints.Employee
 {
   public class List : EndpointBaseAsync
     .WithRequest<EmployeeListRequest>
-    .WithActionResult<List<EmployeeDto>>
+    .WithActionResult<IEnumerable<EmployeeDto>>
   {
     private readonly AppUserManager _appUserManager;
     private readonly IMapper _mapper;
@@ -28,13 +30,24 @@ namespace w9wen.dotnet.Template.Web.Endpoints.Employee
         OperationId = "Employee.List",
         Tags = new[] { "EmployeeEndpoints" })
     ]
-    public override async Task<ActionResult<List<EmployeeDto>>> HandleAsync([FromQuery] EmployeeListRequest request, CancellationToken cancellationToken = default)
+    public override async Task<ActionResult<IEnumerable<EmployeeDto>>> HandleAsync([FromQuery] EmployeeListRequest request, CancellationToken cancellationToken = default)
     {
       var query = _appUserManager.Users;
 
-      if (!string.IsNullOrEmpty(request.Gender)) query = query.Where(x => !string.IsNullOrEmpty(x.Gender) && x.Gender.ToLower() == request.Gender.ToLower());
-      if (!string.IsNullOrEmpty(request.City)) query = query.Where(x => !string.IsNullOrEmpty(x.City) && x.City.ToLower() == request.City.ToLower());
-      if (!string.IsNullOrEmpty(request.Country)) query = query.Where(x => !string.IsNullOrEmpty(x.Country) && x.Country.ToLower() == request.Country.ToLower());
+      #region filter
+
+      if (!string.IsNullOrEmpty(request.Gender))
+        query = query.Where(x => !string.IsNullOrEmpty(x.Gender) && x.Gender.ToLower() == request.Gender.ToLower());
+
+      if (!string.IsNullOrEmpty(request.City))
+        query = query.Where(x => !string.IsNullOrEmpty(x.City) && x.City.ToLower() == request.City.ToLower());
+
+      if (!string.IsNullOrEmpty(request.Country))
+        query = query.Where(x => !string.IsNullOrEmpty(x.Country) && x.Country.ToLower() == request.Country.ToLower());
+
+      #endregion filter
+
+      #region order
 
       query = request.OrderBy switch
       {
@@ -43,9 +56,24 @@ namespace w9wen.dotnet.Template.Web.Endpoints.Employee
         _ => query.OrderByDescending(x => x.UserName)
 
       };
-      var employeeDtoList = _mapper.Map<List<EmployeeDto>>(query.ToList());
 
-      return Ok(employeeDtoList);
+      #endregion order
+
+      var employeeListPaged = await PagedList<EmployeeDto>.CreateAsync(
+        query.ProjectTo<EmployeeDto>(_mapper.ConfigurationProvider).AsNoTracking(),
+        request.PageNumber,
+        request.PageSize);
+
+      Response.AddPaginationHeader(employeeListPaged.CurrentPage,
+                                   employeeListPaged.PageSize,
+                                   employeeListPaged.TotalCount,
+                                   employeeListPaged.TotalPages);
+
+      return Ok(employeeListPaged);
+      // var employeeDtoList = _mapper.Map<List<EmployeeDto>>(query.ToList());
+
+      // Response.AddPaginationHeader(query.cu)
+      // return Ok(employeeDtoList);
 
       // var employeeDtoQuery = query.ProjectTo<List<EmployeeDto>>(_mapper.ConfigurationProvider)
       //                             .AsQueryable();
