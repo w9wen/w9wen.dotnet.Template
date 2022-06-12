00c61ab0cf0a969e09e7781eda1e296c7fbe71d6
w9wen <william.wen@yes-energy.com.tw>
2022-06-12 08:10:35 +0800

    emploee ($Web) Hide the Roles editor if employee is himself.

--- src/w9wen.dotnet.Template.Web/ClientApp/src/app/_modules/shared.module.ts
+++ src/w9wen.dotnet.Template.Web/ClientApp/src/app/_modules/shared.module.ts
@@ -10,6 +10,7 @@ import { PaginationModule } from "ngx-bootstrap/pagination";
 import { TabsModule } from "ngx-bootstrap/tabs";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatSelectModule } from "@angular/material/select";
+import { MatListModule } from "@angular/material/list";
 
 @NgModule({
   declarations: [],
@@ -26,7 +27,8 @@ import { MatSelectModule } from "@angular/material/select";
     PaginationModule.forRoot(),
     TabsModule.forRoot(),
     MatFormFieldModule,
-    MatSelectModule
+    MatSelectModule,
+    MatListModule
   ],
   exports: [
     ModalModule,
@@ -38,7 +40,8 @@ import { MatSelectModule } from "@angular/material/select";
     PaginationModule,
     TabsModule,
     MatFormFieldModule,
-    MatSelectModule
+    MatSelectModule,
+    MatListModule
   ]
 })
 export class SharedModule { }
